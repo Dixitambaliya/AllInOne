762a30e9614a171eb1e8442ef8ad1400ff9f03a8
Dixitambaliya <dixitambaliya303@gmail.com>
2024-09-25 23:33:52 +0530

    user model chages

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,21 @@
+// Import and configure dotenv to load environment variables
+require('dotenv').config();
 const mongoose = require('mongoose');
 
+// Use the MONGO_URI environment variable
 const mongoURI = process.env.MONGO_URI;
 
+// Connect to MongoDB using the URI from .env
+mongoose.connect(mongoURI, {
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+// Define your schema
 const userSchema = mongoose.Schema({
-    email:String,
-    password:String,
+    email: String,
+    password: String,
 });
 
-module.exports = mongoose.model('user',userSchema);
+// Export the model
+module.exports = mongoose.model('user', userSchema);

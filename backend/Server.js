6ad98e1e5282d53be5f942a8299f1b646d90fb6f
Dixitambaliya <dixitambaliya303@gmail.com>
2024-09-25 11:22:06 +0530

    cors update

--- backend/Server.js
+++ backend/Server.js
@@ -10,7 +10,9 @@ const secretkey = 'your_secret_key_here'; // Replace this with your actual secre
 const userModel = require('./models/userModel');
 const Result = require('./models/resultModel');
 
-app.use(cors({ origin: 'https://allinone-1.onrender.com', credentials: true }));
+app.use(cors({ origin: 'https://allinone-1.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+   credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
